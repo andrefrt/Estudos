9cae344669bf84baa921103f435101d8aaf6cc84
andrefrt <116937559+andrefrt@users.noreply.github.com>
2022-10-29 15:45:12 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,12 +1,11 @@
-#Programa em que o usuário adivinha um número gerado randomicamente pelo código
-
+#Programa em que o usuário adivinha um número gerado randomicamente pela máquina
 import random
 
 def tentativa(x):
     num_aleatorio = random.randint(1, x)
     n = 0
     while n != num_aleatorio:
-        n = (int(input("Em que número estou pensando?")))
+        n = (int(input("Em que número (maior que zero) estou pensando?")))
 
         if n > num_aleatorio:
             print("Tente novamente, com um número menor!")
